188ce69870e483f17061aecb52239bcbd8ccf8d8
Said Tokmak <tokmaksaid@gmail.com>
2022-07-05 00:35:46 +0300

    [#1] Implement location api crud operations

--- src/main/java/com/said/galerytown/controller/LocationController.java
+++ src/main/java/com/said/galerytown/controller/LocationController.java
@@ -1,10 +1,12 @@
 package com.said.galerytown.controller;
 
 import com.said.galerytown.entity.Location;
+import com.said.galerytown.exception.ApiException;
 import com.said.galerytown.service.ILocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +28,21 @@ public class LocationController {
     @RequestMapping(value = "/location", produces = {"application/json"}, method = RequestMethod.POST)
     public ResponseEntity<Location> insertLocation(@RequestBody Location location) {
         return ResponseEntity.ok(locationService.insertLocation(location));
+    }
+
+    @RequestMapping(value = "/location/{location_id}", produces = {"application/json"}, method = RequestMethod.GET)
+    public ResponseEntity<Location> getLocation(@PathVariable("location_id") int locationId) {
+        return ResponseEntity.ok(locationService.getLocation(locationId));
+    }
+
+    @RequestMapping(value = "/location/{location_id}", produces = {"application/json"}, method = RequestMethod.PUT)
+    public ResponseEntity<Location> updateLocation(@PathVariable("location_id") int locationId, @RequestBody Location location) throws ApiException {
+        return ResponseEntity.ok(locationService.updateLocation(locationId, location));
+    }
+
+    @RequestMapping(value = "/location/{location_id}", produces = {"application/json"}, method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteLocation(@PathVariable("location_id") int locationId) {
+        locationService.deleteLocation(locationId);
+        return null;
     }
 }
--- src/main/java/com/said/galerytown/repository/ILocationRepository.java
+++ src/main/java/com/said/galerytown/repository/ILocationRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ILocationRepository extends JpaRepository<Location, Integer> {
 
+    Location findLocationById(int locationId);
 }
